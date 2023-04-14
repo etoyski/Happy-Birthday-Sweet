8b458c87e7d560020a341ba106dd575db125e0e9
unknown <solannar03@gmail.com>
2023-04-14 09:43:28 +0800

    edit

--- birthday-counter-master/scripts/index.js
+++ birthday-counter-master/scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  birthdate: 'Apr 23, 2023',
+  name: 'Sweet'
 };
 
 function hideEverything() {
